ef2101c5771377ed35aa6dde3c517eaf48ad71c8
Pierry Borges <pieerry@gmail.com>
2016-07-23 22:01:38 -0300

    Added home pager

--- app/src/main/java/com/github/pierry/noute/MainActivity.java
+++ app/src/main/java/com/github/pierry/noute/MainActivity.java
@@ -1,8 +1,56 @@
 package com.github.pierry.noute;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
-public class MainActivity extends AppCompatActivity {
-  
+@EActivity(R.layout.activity_main) public class MainActivity extends AppCompatActivity {
+
+  @ViewById PagerSlidingTabStrip tabs;
+  @ViewById ViewPager pager;
+
+  @Bean(ToolbarBase.class) ToolbarBase toolbarBase;
+
+  @AfterViews void init() {
+    SystemBarTintManager tintManager = new SystemBarTintManager(getActivity());
+    tintManager.setStatusBarTintEnabled(true);
+    pager.setAdapter(new HomeAdapter(getActivity().getSupportFragmentManager()));
+    tabs.setViewPager(pager);
+    tabs.setTextColorResource(R.color.silver);
+    tabs.setDividerColor(android.R.color.transparent);
+    final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4,
+        getResources().getDisplayMetrics());
+    pager.setPageMargin(pageMargin);
+    pager.setCurrentItem(0);
+    toolbarBase.changeColor(tabs, tintManager, getResources().getColor(R.color.colorPrimary));
+    tabs.setOnPageChangeListener(this);
+  }
+
+  @Override
+  public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    LinearLayout layout = (LinearLayout) tabs.getChildAt(0);
+    for (int i = 0; i < layout.getChildCount(); i++) {
+      TextView tv = (TextView) layout.getChildAt(i);
+      if (i == position) {
+        tv.setTextColor(getResources().getColor(R.color.icons));
+      } else {
+        tv.setTextColor(getResources().getColor(R.color.silver));
+      }
+    }
+  }
+
+  @Override public void onPageSelected(int position) {
+
+  }
+
+  @Override public void onPageScrollStateChanged(int state) {
+
+  }
+
 }
---
+++ app/src/main/java/com/github/pierry/noute/ui/common/ToolbarBase.java
@@ -0,0 +1,85 @@
+package com.github.pierry.noute.ui.common;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import com.astuetz.PagerSlidingTabStrip;
+import com.github.pierry.cartolapp.MainActivity_;
+import com.github.pierry.cartolapp.R;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+import org.androidannotations.annotations.EBean;
+import org.androidannotations.annotations.UiThread;
+
+@EBean public class ToolbarBase {
+
+  private Drawer drawer;
+  private Toolbar toolbar;
+  private Activity act;
+  private AccountHeader headerResult;
+  private Context context;
+
+  public ToolbarBase(Context context) {
+    this.context = context;
+  }
+
+  @UiThread public void injectToolbar(Toolbar toolbar, Activity act) {
+    this.toolbar = toolbar;
+    this.act = act;
+    initAccount();
+    initDrawer();
+  }
+
+  @UiThread void initDrawer() {
+    PrimaryDrawerItem home =
+        new PrimaryDrawerItem().withName(R.string.home).withIcon(GoogleMaterial.Icon.gmd_home);
+    new DrawerBuilder().withActivity((Activity) act)
+        .withToolbar(toolbar)
+        .withHasStableIds(true)
+        .withTranslucentStatusBar(true)
+        .withSelectedItem(-1)
+        .withAccountHeader(headerResult)
+        .addDrawerItems(home)
+        .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+          @Override public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+            switch (position) {
+              case 1:
+                context.startActivity(new Intent(context, MainActivity_.class));
+                return true;
+              default:
+                return true;
+            }
+          }
+        })
+        .build();
+  }
+
+  @UiThread void initAccount() {
+    headerResult = new AccountHeaderBuilder().withActivity(act)
+        .withHeaderBackground(R.color.colorPrimary)
+        .addProfiles(new ProfileDrawerItem().withName("Usuário"))
+        .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+          @Override
+          public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+            return false;
+          }
+        })
+        .build();
+  }
+
+  @UiThread
+  public void changeColor(PagerSlidingTabStrip tabs, SystemBarTintManager tintManager, int newColor) {
+    tabs.setBackgroundColor(newColor);
+    tintManager.setTintColor(newColor);
+  }
+}
