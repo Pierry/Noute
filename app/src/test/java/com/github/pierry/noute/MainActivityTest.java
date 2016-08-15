3925df235702e17f22809a42856047ecbae2969e
Pierry Borges <pieerry@gmail.com>
2016-08-15 14:52:02 -0300

    Add Roboletric

---
+++ app/src/test/java/com/github/pierry/noute/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.github.pierry.noute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class) @Config(constants = BuildConfig.class)
+public class MainActivityTest {
+
+  @Test public void addClick() {
+  }
+}
