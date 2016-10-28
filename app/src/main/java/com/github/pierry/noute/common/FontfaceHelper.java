aacf18d1ce222f4161b8cc0ae3832c8e792dfa4f
Pierry Borges <pieerry@gmail.com>
2016-10-28 17:40:02 -0200

    Update layout

--- app/src/main/java/com/github/pierry/noute/common/FontfaceHelper.java
+++ app/src/main/java/com/github/pierry/noute/common/FontfaceHelper.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 public class FontfaceHelper {
 
   public static void setFontFace(Context context, TextView textView) {
-    Typeface type = Typeface.createFromAsset(context.getAssets(), "OpenSans-Semibold.ttf");
+    Typeface type = Typeface.createFromAsset(context.getAssets(), "droidsans.ttf");
     textView.setTypeface(type);
   }
 }
